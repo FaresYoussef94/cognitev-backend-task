0e63f5b63c511d638fab1763357f13c2b7da4eab
Fares Youssef <fares.youssef@seal-software.com>
2019-09-15 13:44:05 +0200

    spring boot setup

---
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitevbackendtask/CognitevBackendTaskApplication.java
@@ -0,0 +1,13 @@
+package com.fares.youssef.cognitevbackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitevBackendTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitevBackendTaskApplication.class, args);
+	}
+
+}
