7919e6c8f00a7af9ae2d85783f46a771f07840f7
Fares Youssef <fares.youssef@seal-software.com>
2019-09-15 14:12:54 +0200

    adding the registration model

---
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/model/RegistrationModel.java
@@ -0,0 +1,81 @@
+package com.fares.youssef.cognitev.backend.task.model;
+
+import java.io.File;
+import java.util.Date;
+
+public class RegistrationModel {
+
+	private String firstName;
+	private String lastName;
+	private String countryCode;
+	private String phoneNumber;
+	private String gender;
+	private Date dateOfBirth;
+	private File avatar;
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public File getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(File avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
