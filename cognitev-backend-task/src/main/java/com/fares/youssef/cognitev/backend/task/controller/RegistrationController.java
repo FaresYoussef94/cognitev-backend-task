cde24f33531b2adcdb00e5127f8f520ac8a0e6ca
Fares Youssef <fares.youssef@seal-software.com>
2019-09-15 22:40:52 +0200

    calling the service and returning the updated data

--- cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/controller/RegistrationController.java
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/controller/RegistrationController.java
@@ -2,27 +2,34 @@ package com.fares.youssef.cognitev.backend.task.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fares.youssef.cognitev.backend.service.RegistrationService;
 import com.fares.youssef.cognitev.backend.task.model.RegistrationModel;
-import com.fares.youssef.cognitev.backend.task.model.ResponseEntity;
 
 @RestController
 @RequestMapping("/registration")
 public class RegistrationController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RegistrationController.class);
+	private final RegistrationService registrationService;
 
-	public RegistrationController() {
-
+	public RegistrationController(RegistrationService registrationService) {
+		this.registrationService = registrationService;
 	}
 
-	@PostMapping()
-	public ResponseEntity registerData(@RequestBody RegistrationModel registrationModel) {
-		return null;
+	@PostMapping
+	public ResponseEntity registerData(@RequestBody(required = false) RegistrationModel registrationModel) {
+		LOG.info("POST - registerData endpoint");
+
+		RegistrationModel registeredData = registrationService.registerData(registrationModel);
+
+		return ResponseEntity.status(HttpStatus.CREATED.value()).body(registeredData);
 	}
 
 }
