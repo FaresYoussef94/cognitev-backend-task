eb0ba9c36a1e67de6f7ede18b1e0068a64c8e9a8
Fares Youssef <fares.youssef@seal-software.com>
2019-09-17 00:41:30 +0200

    removing the bean creation to use the sterotype annotations instead

--- cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/CognitevBackendTaskApplication.java
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/CognitevBackendTaskApplication.java
@@ -2,21 +2,12 @@ package com.fares.youssef.cognitev.backend.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.fares.youssef.cognitev.backend.service.RegistrationService;
-import com.fares.youssef.cognitev.backend.service.RegistrationServiceImpl;
 
 @SpringBootApplication
 public class CognitevBackendTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CognitevBackendTaskApplication.class, args);
-	}
-
-	@Bean
-	public RegistrationService getRegistrationService() {
-		return new RegistrationServiceImpl();
 	}
 
 }
