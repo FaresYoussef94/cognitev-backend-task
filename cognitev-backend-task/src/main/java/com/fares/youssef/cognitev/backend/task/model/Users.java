390b9d2962cace279c2cff447df2940466930668
Fares Youssef <fares.youssef@seal-software.com>
2019-09-18 18:22:21 +0200

    adding the user model

---
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/model/Users.java
@@ -0,0 +1,47 @@
+package com.fares.youssef.cognitev.backend.task.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+@Entity
+@Table(name = "users")
+public class Users {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String phoneNumber;
+	private String password;
+	private String token;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
