4bc3a91e5fb547d1f5c6cfed8ce334749c0a8a58
Fares Youssef <fares.youssef@seal-software.com>
2019-09-19 00:05:57 +0200

    getting users object instead of optional

--- cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/repository/UserModelRepository.java
+++ cognitev-backend-task/src/main/java/com/fares/youssef/cognitev/backend/task/repository/UserModelRepository.java
@@ -1,19 +1,16 @@
 package com.fares.youssef.cognitev.backend.task.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.fares.youssef.cognitev.backend.task.model.Users;
 
 public interface UserModelRepository extends JpaRepository<Users, String> {
 
 //	@Query(value = "SELECT u FROM Users u where u.phone_number = ?1 and u.password = ?2 ")
-	Optional<Users> findAllByPhoneNumberAndPassword(String username, String password);
+	Users findAllByPhoneNumberAndPassword(String username, String password);
 
-	Optional<Users> findByToken(String token);
+	Users findByToken(String token);
 
-	Optional<Users> findById(Long id);
+	Users findById(Long id);
 
 }
